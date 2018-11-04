ad70afc83ae9a2c52989f23039e3b1e1e12a5a1b
Виталий Никонов <BarsStudio@gmail.com>
2018-11-04 15:29:17 +0800

    DZ3

---
+++ Scripts/Models/TeammateModel.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+namespace Geekbrains
+{
+
+
+    public class TeammateModel : BaseObjectScene
+    {
+
+        private NavMeshAgent _agent;             // the navmesh agent required for the path finding
+        private ThirdPersonCharacter _character; // the character we are controlling
+        public bool IsRun { get; set; }
+        //public Transform target;                                    // target to aim for
+
+
+        private void Start()
+        {
+            _agent = GetComponent<NavMeshAgent>();
+            _character = GetComponent<ThirdPersonCharacter>();
+
+
+
+            _agent.updatePosition = true ;
+            _agent.updateRotation = false;
+        }
+
+        private void Update()
+        {
+            if (_agent.remainingDistance > _agent.stoppingDistance)
+            {
+                _character.Move(_agent.desiredVelocity, false, false);
+                IsRun = true;
+            }
+                
+            else { 
+                _character.Move(Vector3.zero, false, false);
+                IsRun = false;
+
+            }
+
+        }
+
+        public void SetDestination(Vector3 pos)
+        {
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(pos, out hit, 5f, -1))
+            {
+                _agent.SetDestination(hit.position);
+
+            }
+            else
+            {
+                Debug.Log("Позиция недостижима");
+            }
+        }
+    }
+
+
+    
+}
---
+++ Scripts/Weapons/MultiBarrelesFireWeapon.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Geekbrains
+{
+
+    public class MultiBarrelesFireWeapon : BaseWeapon
+    {
+
+        [SerializeField]
+        private Transform[] _firepoints;
+        private int _currentfirepoint;
+
+        private void Awake()
+        {
+            _ammocurent = _ammomax;
+        }
+
+        public override void Fire()
+        {
+            if (!TryShoot()) return;
+
+            BaseAmmo bullet = Instantiate(_bulletPrefab);
+            bullet.Initialize(_firepoints[_currentfirepoint], _force);
+
+            _currentfirepoint++;
+            if (_currentfirepoint >= _firepoints.Length) _currentfirepoint = 0;
+        }
+
+        public override void Reload()
+        {
+            IsReloading = true;
+            _lastreloadingtime = Time.time;
+            Debug.Log("Пошла перезарядка");
+        }
+
+        private void Update()
+        {
+            //Сработка перезарядки
+            if (IsReloading && Time.time - _lastreloadingtime > _reloadTime)
+            {
+                _ammocurent = _ammomax;
+                IsReloading = false;
+            }
+        }
+    }
+}
